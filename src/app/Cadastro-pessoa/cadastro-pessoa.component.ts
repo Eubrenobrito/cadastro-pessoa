fe3102a3042b63a3563dd00306509f5e41ee05da
Eubrenobrito <brenobrito_13@hotmail.com>
2022-09-25 11:41:52 -0300

    adicionando componente angular material, tela cadastro

---
+++ src/app/Cadastro-pessoa/cadastro-pessoa.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-cadastro-pessoa',
+  templateUrl: './cadastro-pessoa.component.html',
+  styleUrls: ['./cadastro-pessoa.component.css']
+})
+
+
+export class CadastroPessoaComponent implements OnInit {
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CadastroPessoaComponent} from "./Cadastro-pessoa/cadastro-pessoa.component";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MAT_DATE_FORMATS, MatNativeDateModule, NativeDateModule} from "@angular/material/core";
+import {MatInputModule} from "@angular/material/input";
+import {MatStepperModule} from "@angular/material/stepper";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
+import {MatSliderModule} from "@angular/material/slider";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroPessoaComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    NativeDateModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MatSliderModule,
+
   ],
-  providers: [],
+  providers: [
+    CadastroPessoaComponent,
+    MatStepperModule,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
